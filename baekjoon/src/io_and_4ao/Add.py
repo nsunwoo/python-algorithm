68963c03511714d64ea6c138babfc599c2ef99fe
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 01:45:58 +0900

    feat: Add problem url

--- baekjoon/src/io_and_4ao/Add.py
+++ baekjoon/src/io_and_4ao/Add.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/1000
+
 a, b = map(int, input().split())
 
 print(a + b)
--- baekjoon/src/io_and_4ao/Subtract.py
+++ baekjoon/src/io_and_4ao/Subtract.py
@@ -1,2 +1,5 @@
+# https://www.acmicpc.net/problem/1001
+
 a, b = map(int, input().split())
+
 print(a - b)
