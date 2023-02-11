6e7e77cbd902db5af0a2efb71f345611ae0cae00
nsunwoo <sunwoo.stats@gmail.com>
2023-02-11 17:29:35 +0900

    BOJ 10807: Count

---
+++ baekjoon/src/array_1d/Count.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/10807
+
+n = int(input())
+a = list(map(int, input().split()))
+v = int(input())
+count = 0
+
+for i in range(n):
+    if a[i] == v:
+        count += 1
+
+print(count)
